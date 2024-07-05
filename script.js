2cb7fd865fe22f6f90f18f0e2f08ef21291683e4
gabrieleinsfeld <gabrieleinsfeld@gmail.com>
2024-07-05 14:47:13 -0300

    Added event listeners to columns Added button to create new grid Added style to grid and button

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-console.log('hello')
+const newGridBtn = document.getElementById("new_grid")
+
+
+
+newGridBtn.addEventListener('click', ()=>{
+    while(true){
+        num = parseInt(prompt('Size of the new grid (1-100)'))
+        if (num >= 1 && num <= 100){
+            cleanGrid()
+            create_grid(num)
+            break
+        }else{
+            alert('NUmber has to be between 1-100')
+        }
+    }
+})
 
 function create_grid(n){
     for(let i=0; i < n; i++){
@@ -9,9 +24,19 @@ function create_grid(n){
         for(let j=0; j < n; j++){
             const column = document.createElement('div')
             column.classList.add('column')
+            column.addEventListener('mouseover', ()=>{
+                column.style.backgroundColor = 'gray'
+            })
             document.getElementById(`row${i}`).appendChild(column)
         }
     }
 }
 
+function cleanGrid(){
+    document.getElementById('container').innerHTML = ''
+}
+
+
+
 create_grid(16)
+
